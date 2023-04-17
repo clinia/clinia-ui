d1b0b959cf1da04f86ffbcf1169dba215d9f0eba
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.com>
2023-04-17 12:29:16 -0500

    chore: setup react package

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,7 @@
+// Grid
+export { Grid, GridItem } from '@chakra-ui/react'
+export type {GridProps, GridItemProps} from '@chakra-ui/react'
+
+// Simple Grid
+export { SimpleGrid } from '@chakra-ui/react'
+export type {SimpleGridProps} from '@chakra-ui/react'
