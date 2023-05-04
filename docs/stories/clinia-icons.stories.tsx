5d7a1cfb35af16ffa02de8aafe0fca0129582afc
Marilou Farmer <marilou.farmer@clinia.com>
2023-05-04 09:52:10 -0400

    Copy icon to clipboard

--- docs/stories/clinia-icons.stories.tsx
+++ docs/stories/clinia-icons.stories.tsx
@@ -1,8 +1,16 @@
-import React, { ComponentType } from "react";
-import { CliniaIcon } from "@clinia-ui/icons/CliniaIcon";
 import { AccessibilityIcon } from "@clinia-ui/icons/AccessibilityIcon";
-import { Card, CardBody, Center, Icon, Stack, Text } from "@clinia-ui/react";
+import { CliniaIcon } from "@clinia-ui/icons/CliniaIcon";
+import {
+  Button,
+  Card,
+  CardBody,
+  Center,
+  Stack,
+  Text,
+  Tooltip,
+} from "@clinia-ui/react";
 import { Meta } from "@storybook/react";
+import { useState } from "react";
 
 const a: Meta = {
   decorators: [(story) => <>{story.toString()}</>],
@@ -10,12 +18,21 @@ const a: Meta = {
 
 type IconStory = () => [string, JSX.Element];
 
-export default {
-  title: "Icons",
-  decorators: [
-    (Story: IconStory) => {
-      const [name, component] = Story();
-      return (
+const IconDecorator = ({ story }: { story: IconStory }) => {
+  const [name, component] = story();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Tooltip isOpen={open} label={`'${name}' copied to clipboard`}>
+      <Button
+        height="fit-content"
+        variant="unstyled"
+        onClick={() => {
+          navigator.clipboard.writeText(name);
+          setOpen(true);
+          setTimeout(() => setOpen(false), 2000);
+        }}
+      >
         <Card align="center">
           <CardBody>
             <Stack>
@@ -24,9 +41,14 @@ export default {
             </Stack>
           </CardBody>
         </Card>
-      );
-    },
-  ],
+      </Button>
+    </Tooltip>
+  );
+};
+
+export default {
+  title: "Icons",
+  decorators: [(story: IconStory) => <IconDecorator story={story} />],
 };
 
 const props = {
