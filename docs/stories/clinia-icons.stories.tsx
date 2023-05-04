7f80b5fa9ae9095d7898574b6158a86327e17ae8
Marilou Farmer <marilou.farmer@clinia.com>
2023-05-04 09:25:43 -0400

    Update icons decorator

--- docs/stories/clinia-icons.stories.tsx
+++ docs/stories/clinia-icons.stories.tsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { ComponentType } from "react";
 import { CliniaIcon } from "@clinia-ui/icons/CliniaIcon";
 import { AccessibilityIcon } from "@clinia-ui/icons/AccessibilityIcon";
+import { Card, CardBody, Center, Icon, Stack, Text } from "@clinia-ui/react";
+import { Meta } from "@storybook/react";
+
+const a: Meta = {
+  decorators: [(story) => <>{story.toString()}</>],
+};
+
+type IconStory = () => [string, JSX.Element];
 
 export default {
   title: "Icons",
+  decorators: [
+    (Story: IconStory) => {
+      const [name, component] = Story();
+      return (
+        <Card align="center">
+          <CardBody>
+            <Stack>
+              <Center>{component}</Center>
+              <Text>{name}</Text>
+            </Stack>
+          </CardBody>
+        </Card>
+      );
+    },
+  ],
 };
 
 const props = {
   color: "primary.main",
-  boxSize: "30px",
+  boxSize: "2em",
 };
 
-export const CliniaIcon_ = () => <CliniaIcon {...props} />;
+export const CliniaIcon_: IconStory = () => [
+  "CliniaIcon",
+  <CliniaIcon {...props} />,
+];
 
-export const AccessibilityIcon_ = () => <AccessibilityIcon {...props} />;
+export const AccessibilityIcon_ = () => [
+  "AccessibilityIcon",
+  <AccessibilityIcon {...props} />,
+];
